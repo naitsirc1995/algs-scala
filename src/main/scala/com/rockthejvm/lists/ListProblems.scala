89cd42cddd309def26e9070393bed981d85af335
Cristian David Chavez Quintero <cristian.chavez@jikkosoft.com>
2022-06-11 19:50:54 -0500

    End of the video

--- src/main/scala/com/rockthejvm/lists/ListProblems.scala
+++ src/main/scala/com/rockthejvm/lists/ListProblems.scala
@@ -6,6 +6,8 @@ sealed abstract class RList[+T] {
   def head:T
   def tail:RList[T]
   def isEmpty:Boolean
+
+  def ::[S>:T](elem:S):RList[S] = new ::(elem,this)
 }
 
 case object RNill extends RList[Nothing] {
@@ -14,9 +16,10 @@ case object RNill extends RList[Nothing] {
   override def isEmpty: Boolean = true
 
   override def toString: String = "[]"
+
 }
 
-case class Cons[+T](override val head:T,override val tail:RList[T]) extends RList[T] {
+case class ::[+T](override val head:T, override val tail:RList[T]) extends RList[T] {
   override def isEmpty: Boolean = false
 
   override def toString: String = {
@@ -24,7 +27,7 @@ case class Cons[+T](override val head:T,override val tail:RList[T]) extends RLis
     def toStringTailRec(remaining:RList[T],result:String):String = {
       if (remaining.isEmpty) result
       else if (remaining.tail.isEmpty) s"$result${remaining.head}"
-      else toStringTailRec(remaining.tail,s"$result${remaining.head}")
+      else toStringTailRec(remaining.tail,s"$result${remaining.head}, ")
     }
     "["+toStringTailRec(this,"")+"]"
   }
@@ -32,5 +35,6 @@ case class Cons[+T](override val head:T,override val tail:RList[T]) extends RLis
 }
 
 object ListProblems extends App {
-
+  val aSmallList = 1 :: 2 :: 3 :: RNill // Rnill.::(3).::(2)::.::(1)
+  println(aSmallList)
 }
